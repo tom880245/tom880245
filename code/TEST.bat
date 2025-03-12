2101fe9f1daccde075412141bd4f4be4077d14c4
tom123 <40041106S@gmail.com>
2025-03-12 16:39:27 +0800

    11403

---
+++ code/TEST.bat
@@ -0,0 +1,7 @@
+@echo off
+echo test
+
+:: testest
+ping 127.0.0.1 -n 6 > nul
+
+echo setset
---
+++ code/執行檔.bat
@@ -0,0 +1,9 @@
+@echo on
+cd C:\Users\Administrator\Desktop\常用code\查詢書圖目錄\compelet\code
+timeout /t 1
+
+julia result.jl
+
+pause
+
+
